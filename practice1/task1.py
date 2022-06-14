7977be746eb7f491f77cf1056fbface3f8de7fdc
levchig737 <levchekarev3@gmail.com>
2022-06-14 17:22:37 +0300

    changes

--- practice1/task1.py
+++ practice1/task1.py
@@ -1,8 +1,15 @@
 numbers = [4,-1,7,0,1,2,-1,5]
 lenght = len(numbers)
+s = set()
 
 for i in range(lenght):
-    for j in range(i+1, lenght):
-        for k in range(j+1, lenght-1):
+    for j in range(0, lenght):
+        for k in range(0, lenght-1):
+            if i == j or i == k or j == k:
+                continue
             if numbers[i] + numbers[j] + numbers[k] == 0:
-                print(numbers[i], numbers[j], numbers[k])
+                res = [str(numbers[i]), str(numbers[j]), str(numbers[k])]
+                res.sort()
+                s.add(" ".join(res))
+                
+print(s)
