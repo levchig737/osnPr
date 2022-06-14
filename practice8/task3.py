91564186c32262622e1d85ab931b9bde02a454e4
levchig737 <levchekarev3@gmail.com>
2022-06-14 20:51:49 +0300

    changes

--- practice8/task3.py
+++ practice8/task3.py
@@ -1,25 +1,24 @@
 pairs = [[1,2],[7,8],[4,5]]
 pairs.sort()
+print(pairs)
 
 def search(seq, i, count=1):
     global pairs
-    
+    global maxn
+
     # Цикл прохода по каждому элементу с i
     for j in range(i+1, len(pairs)):
         # Проверка условия
         if seq[1] < pairs[j][0]:
-            count += 1
+            maxn = max(maxn, count+1)
             # С этой пары запускаем рекурсивно функцию
-            search(pairs[j], j, count)
-    return count
-        
+            search(pairs[j], j, count+1)        
 
     
 maxn = -1
 
 for i in range(len(pairs)-1):
-    res = search(pairs[i],i)
-    maxn = max(res, maxn)
+    search(pairs[i],i)
 
 
 print(maxn)
